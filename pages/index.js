dc61f843ae524f7b1d29d4f5dcd297ae0a10eaa7
Tim Nelke <tim.nelke@outlook.com>
2020-05-27 22:51:13 +0200

    added tentative styling for nav and hero sections

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,76 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import Logo from "../components/logo";
+import Navigation from "../components/navigation";
+
+const Avatar = ({ src, alt, size = "md" }) => {
+  const sizeStyle = {
+    sm: "h-12 w-12",
+    md: "h-32 w-32",
+    lg: "h-64 w-64",
+  }[size];
+
+  return <img className={`${sizeStyle} rounded-full`} src={src} alt={alt} />;
+};
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="antialiased">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className="text-2xl">
-          Hello world!
+      <div className="flex items-center justify-between px-12 py-8">
+        <Link href="/">
+          <a className="z-50">
+            <Logo className="h-8 hover:text-foxfire-600" />
+          </a>
+        </Link>
+        <Navigation />
+      </div>
+
+      <main className="grid-cols-12 px-12 space-y-12 mt-24">
+        <div className="col-start-1 col-span-8 flex items-center space-x-4">
+          <h1>
+            <span className="text-transparent text-gradient text-6xl font-bold">
+              Ohayo, I'm Tim Nelke.
+            </span>
+          </h1>
+          <Avatar src="profile.png" />
+        </div>
+        <div className="space-y-4">
+          <div>
+            <p>
+              Based in Willich, Germany, I’m a computer science student at{" "}
+              <a
+                className="font-medium text-foxfire-600 hover:underline"
+                href="https://www.hs-niederrhein.de/"
+              >
+                Hochschule Niederrhein
+              </a>
+              , currently threading the needle between beeing a jack-of-all-trades
+              and becoming an expert, who blends design knowledge and
+              application development.
+            </p>
+          </div>
+          <div className="space-x-1">
+            <Link href="">
+              <a className="font-medium text-foxfire-600 hover:underline">
+                Download my CV
+              </a>
+            </Link>
+            <span>or</span>
+            <Link href="">
+              <a className="font-medium text-foxfire-600 hover:underline">
+                View My Projects
+              </a>
+            </Link>
+          </div>
         </div>
       </main>
 
-      <footer>
-
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
     "./components/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foxfire: {
+          '100': '#fee7eb',
+          '200': '#fbb6c4',
+          '300': '#f9869d',
+          '400': '#f75575',
+          '500': '#f4244e',
+          '600': '#f20c3a',
+          '700': '#db0b34',
+          '800': '#aa0829',
+          '900': '#79061d',
+        }
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/ui")
+  ],
 }
