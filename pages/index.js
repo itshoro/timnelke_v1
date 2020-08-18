df6e6cb58c21860de0b62d66ee11062479e0893c
Tim Nelke <tim.nelke@outlook.com>
2020-08-18 03:43:51 +0200

    Fixed z-index of menu on mobile devices

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       </Head>
 
       <div className="min-h-screen flex flex-col px-8 md:px-24 py-8 z-50">
-        <div className="flex items-center justify-between border-b border-lighter-gray dark:border-off-black py-6 z-10">
+        <div className="flex items-center justify-between border-b border-lighter-gray dark:border-off-black py-6 z-50">
           <Link href="/">
             <a className="z-50">
               <Logo className="h-8 hover:text-foxfire-600" />
