88a02cc03fe3f492e018d337390ddb9282b09890
Tim Nelke <tim.nelke@outlook.com>
2020-08-18 03:31:23 +0200

    Animated terrain can now resize on will

--- components/animatedTerrain.js
+++ components/animatedTerrain.js
@@ -1,47 +1,36 @@
-
 const AnimatedTerrain = ({ className }) => {
-
-  const Sketch = typeof window !== "undefined" ? require("react-p5") : "null";
+  const Sketch = typeof window !== 'undefined' ? require('react-p5') : 'null';
 
   const fac = 2;
-  const scale = 50;
+  const scale = 55;
   let rows, cols;
 
   let terrain = [];
   let alpha = [];
   let fadeIn = 0;
   let velocityY = 0;
-  
-  let context;
 
-  const isWindowDefined = typeof window !== "undefined";
+  const isWindowDefined = typeof window !== 'undefined';
 
-  const h = isWindowDefined ? window.innerHeight / 2 : 1;
-  const w = isWindowDefined ? window.innerWidth / 2 : 1;
-
+  let h = isWindowDefined ? window.innerHeight / 2 : 1;
+  let w = isWindowDefined ? window.innerWidth / 2 : 1;
 
   const LIGHT = 0;
   const DARK = 1;
-  const colors = [
-    0,
-    255
-  ];
+  const colors = [0, 255];
   let activeColor = LIGHT;
 
   let media;
   if (isWindowDefined)
-    media = window.matchMedia("(prefers-color-scheme: dark)");
+    media = window.matchMedia('(prefers-color-scheme: dark)');
 
   const changeColorScheme = (media) => {
     activeColor = media.matches ? DARK : LIGHT;
-  }
+  };
 
-  const setup = (p, parentRef) => {
-    context = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL).parent(parentRef);
-    // context.canvas.classList.add("opacity-0");
-
-    rows = Math.ceil(h / scale);
-    cols = Math.ceil(w / scale);
+  const generateData = (p) => {
+    rows = Math.ceil(h / scale) + 2;
+    cols = Math.ceil(w / scale) + 2;
     for (let y = 0; y < rows; y++) {
       terrain[y] = [];
       alpha[y] = p.map(y, 0, rows - 1, 5, 15);
@@ -51,51 +40,73 @@ const AnimatedTerrain = ({ className }) => {
       //   alpha[y][x] = p.map(y, 0, rows - 1, 5, 15) * p.map((x / cols / 2) % 2, 0, 1, 0, 1);
       // }
     }
+  };
 
-      changeColorScheme(media);
-      media.addListener(changeColorScheme);
-  }
+  const setup = (p, parentRef) => {
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL).parent(parentRef);
 
+    generateData(p);
+
+    changeColorScheme(media);
+    media.addListener(changeColorScheme);
+  };
+
+  const windowResized = (p) => {
+    h = isWindowDefined ? window.innerHeight / 2 : 1;
+    w = isWindowDefined ? window.innerWidth / 2 : 1;
+    generateData(p);
+
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 
   const draw = (p) => {
     p.background(colors[(activeColor + 1) % colors.length]);
-    
+
     velocityY -= 0.005;
 
     p.translate(0, 50);
     p.rotateX(p.PI / 3);
 
-    p.translate(-w, -h);
-    
+    p.translate(-w * 1.125, -h);
+
     let yOffset = velocityY;
     for (let y = 0; y < rows - 1; y++) {
       let xOffset = 0;
 
       p.fill(colors[activeColor], alpha[y] * fadeIn * (1 + activeColor * 0.5));
-      p.stroke(colors[activeColor], alpha[y] * fadeIn * (1 + activeColor * 0.5));
+      p.stroke(
+        colors[activeColor],
+        alpha[y] * fadeIn * (1 + activeColor * 0.5)
+      );
 
       p.beginShape(p.TRIANGLE_STRIP);
       for (let x = 0; x < cols; x++) {
-        if(y % 2 == 0) {
-          terrain[y][x] = p.map(p.noise(xOffset, yOffset), 0, 1, -150, 100);
-          terrain[y + 1][x] = p.map(p.noise(xOffset, yOffset + 0.2), 0, 1, -150, 100);
-
+        if (y % 2 == 0) {
+          terrain[y][x] =
+            p.map(p.noise(xOffset, yOffset), 0, 1, -150, 100) * fac;
+          terrain[y + 1][x] =
+            p.map(p.noise(xOffset, yOffset + 0.2), 0, 1, -150, 100) * fac;
         }
         xOffset += 0.3;
 
-        p.vertex(x * scale * fac, y * scale * fac, terrain[y][x] * fac);
-        p.vertex(x * scale * fac, (y + 1) * scale * fac, terrain[y + 1][x] * fac);
+        p.vertex(x * scale * fac, y * scale * fac, terrain[y][x]);
+        p.vertex(x * scale * fac, (y + 1) * scale * fac, terrain[y + 1][x]);
       }
-      p.endShape()
+      p.endShape();
       yOffset += 0.2;
     }
 
-    if (fadeIn < 1)
-      fadeIn += 0.025;
+    if (fadeIn < 1) fadeIn += 0.025;
+  };
 
-  }
-
-  return <Sketch className={className} setup={setup} draw={draw}></Sketch>
+  return (
+    <Sketch
+      className={className}
+      setup={setup}
+      draw={draw}
+      windowResized={windowResized}
+    />
+  );
 };
 
-export default AnimatedTerrain
+export default AnimatedTerrain;
